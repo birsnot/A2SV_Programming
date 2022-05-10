39470094536443ec5db025ae75b207ebe5fa4e7a
birsnot <104576546+birsnot@users.noreply.github.com>
2022-05-10 14:37:37 -0300

    Create Python_If-Else.py

---
+++ Python_If-Else/Python_If-Else.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n in range(6,21) or n%2 == 1:
+    print('Weird')
+else:
+    print('Not Weird')
