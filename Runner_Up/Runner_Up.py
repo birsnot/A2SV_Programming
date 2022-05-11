c036c9b3aa5f12e49217e5e22005d0403e36e077
birsnot <104576546+birsnot@users.noreply.github.com>
2022-05-11 17:23:07 -0300

    Create Runner_Up.py

---
+++ Runner_Up/Runner_Up.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    max1 = -101
+    for i in arr:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2 and i != max1:
+            max2 = i
+    print(max2)
