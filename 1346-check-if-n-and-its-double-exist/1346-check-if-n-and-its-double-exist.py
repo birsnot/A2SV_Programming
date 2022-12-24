7f333c1e0aa6f4cd2b50842bff84dbf0172db9ce
birsnot <104576546+birsnot@users.noreply.github.com>
2022-12-24 05:47:25 -0300

    Time: 53 ms (94.87%), Space: 14 MB (15.36%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        cases = set()
+        
+        for n in arr:
+            if n in cases:
+                return True
+            cases.add(n*2)
+            cases.add(n/2)
+        return False
