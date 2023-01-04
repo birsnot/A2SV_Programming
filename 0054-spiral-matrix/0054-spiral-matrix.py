32d058c57029620d158a0369f2d273b876d7daae
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-04 12:53:45 -0300

    Time: 39 ms (63.86%), Space: 13.9 MB (82.32%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -8,13 +8,11 @@ class Solution:
         right_end = n-1
         bottom_end = m-1
         left_end = 0
-        while True:
+        while len(ans) != ans_len:
             # move down
             for row in range(top_end, bottom_end+1):
                 ans.append(matrix[row][right_end])
             right_end -= 1
-            if len(ans) == ans_len:
-                break
                 
             #move_left
             for col in range(right_end, left_end-1, -1):
@@ -27,13 +25,9 @@ class Solution:
             for row in range(bottom_end, top_end-1, -1):
                 ans.append(matrix[row][left_end])
             left_end += 1
-            if len(ans) == ans_len:
-                break
             
             #move_right
             for col in range(left_end, right_end+1):
                 ans.append(matrix[top_end][col])
             top_end += 1
-            if len(ans) == ans_len:
-                break
         return ans
