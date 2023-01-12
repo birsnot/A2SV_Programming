2114af3f97980801800368d2e1def6ec9ab7e411
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-12 13:56:22 -0300

    Time: 56 ms (89.29%), Space: 14 MB (75.14%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -4,25 +4,25 @@ class Solution:
             return s
         jump1 = numRows*2 - 2
         jump2 = 0
-        ans = []
+        ans = ""
         str_len = len(s)
         for r in range(numRows):
             i = r
             if jump1 and jump2:
                 while i < str_len:
-                    ans.append(s[i])
+                    ans += s[i]
                     i += jump1
                     if i < str_len:
-                        ans.append(s[i])
+                        ans += s[i]
                         i += jump2
             elif jump1:
                 while i < str_len:
-                    ans.append(s[i])
+                    ans += s[i]
                     i += jump1
             else:
                 while i < str_len:
-                    ans.append(s[i])
+                    ans += s[i]
                     i += jump2
             jump1 -= 2
             jump2 += 2
-        return "".join(ans)
+        return ans
