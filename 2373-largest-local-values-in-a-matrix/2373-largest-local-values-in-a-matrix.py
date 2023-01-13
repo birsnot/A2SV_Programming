458f960722a118b816e17b7ba4fe85dc13bb2f7e
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-13 19:36:08 -0300

    Time: 131 ms (99.88%), Space: 14.4 MB (40.34%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -3,15 +3,11 @@ class Solution:
         n = len(grid)
         ans_len = n - 2
         ans = [[0]*(ans_len) for _ in range(ans_len)]
-        for r, row in enumerate(grid):
-            for c, val in enumerate(row):
-                r_left = max(0, r - 2)
-                r_right = min(ans_len, r + 1)
-                c_left = max(0, c - 2)
-                c_right = min(ans_len, c + 1)
-                for r_ in range(r_left, r_right):
-                    for c_ in range(c_left, c_right):
-                        ans[r_][c_] = max(ans[r_][c_], val)
-        return ans
-                
         
+        for r in range(ans_len):
+            for c in range(ans_len):
+                ans[r][c] = max(grid[r][c],grid[r][c + 1],grid[r][c + 2],
+                                grid[r + 1][c],grid[r + 1][c + 1],grid[r + 1][c + 2],
+                                grid[r + 2][c],grid[r + 2][c + 1],grid[r + 2][c + 2],
+                               )
+        return ans
