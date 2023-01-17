a42a6ab14960a0433bf6e26fec7bfd667c9b28cb
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-17 12:16:02 -0300

    Time: 107 ms (98.79%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,8 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n = len(heights)
-        names_ = [""]*n
-        sorted_idx = sorted(range(n), key=lambda i: heights[i], reverse=True)
-        for i, idx in enumerate(sorted_idx):
-            names_[i] = names[idx]
-        return names_
+        return [name for _, name in sorted(zip(heights, names), reverse=True)]
