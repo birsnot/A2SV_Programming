3cf4df5874df1c1a9aa7ff8fcba47d9e0c34af19
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-19 16:04:43 -0300

    Time: 47 ms (99.15%), Space: 13.8 MB (83.54%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        freq = [0]*101
+        max_ = max(nums) + 1
+        freq = [0]*max_
         for n in nums:
             freq[n] += 1
         prev = 0
-        for i in range(101):
+        for i in range(max_):
             val = freq[i]
             freq[i] = prev
             prev += val
