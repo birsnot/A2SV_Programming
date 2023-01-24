cab490fcaae1b47950ea2e8e7ce59a735157f843
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-24 19:44:13 -0300

    Time: 261 ms (33.40%), Space: 17 MB (23.68%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,49 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        change = [0]*len(ratings)
+        for i, n in enumerate(ratings[1:]):
+            if ratings[i] < n:
+                change[i+1] = change[i] + 1
+            elif ratings[i] > n:
+                change[i+1] = change[i] - 1
+            else:
+                change[i+1] = change[i]
+        pivots = []
+        prev = "a"
+        for i, n in enumerate(change):
+            if n == prev:
+                pivots.append(i)
+            prev = n
+        pivots.append(len(ratings))
+        ans = [0]*len(ratings)
+        prev_p = 0
+        for p in pivots:
+            mounts = []
+            i = prev_p + 1
+            up = False
+            while i < p:
+                if not up and change[i] > change[i-1]:
+                    up = True
+                elif up and change[i] < change[i-1]:
+                    up = False
+                    mounts.append(i)
+                i += 1
+            mounts.append(p)
+            prev_m = prev_p
+            for m in mounts:
+                sub = change[prev_m:m]
+                min_ = min(sub)
+                i = prev_m
+                while i < m:
+                    ans[i] = max(ans[i], change[i] - min_)
+                    i += 1
+                prev_m = m - 1
+            if prev_p < len(ratings)-1 and change[prev_p] < change[prev_p + 1]:
+                ans[prev_p] = 0
+            if p > 1 and change[p-1] < change[p-2]:
+                change[p-1] = 0
+                
+            prev_p = p
+        
+        
+        return sum(ans) + len(ratings)
