3731ec1ec255f9466d6df3ccfa9b6bd5e1e3ae8e
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-25 13:59:01 -0300

    Time: 423 ms (40.71%), Space: 13.8 MB (97.15%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,15 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.squares = {}
-        for n in range(46341):
-            self.squares[n*n] = 0
-            
     def judgeSquareSum(self, c: int) -> bool:
-        loops = int(sqrt(c)) + 1
-        p = iter(self.squares)
-        for i in range(loops):
-            a2 = next(p)
-            b2 = c - a2
-            if b2 in self.squares:
+        right = int(sqrt(c))
+        left = 0
+        while left <= right:
+            sum_ = left**2 + right**2
+            if sum_ == c:
                 return True
+            elif sum_ > c:
+                right -= 1
+            else:
+                left += 1
         return False
