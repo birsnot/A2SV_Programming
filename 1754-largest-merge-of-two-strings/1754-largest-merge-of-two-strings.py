544f783a800fa838fe9fdbec24d1027458c3c685
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-01-27 05:49:54 -0300

    Time: 82 ms (88.52%), Space: 14 MB (86.07%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -6,22 +6,10 @@ class Solution:
         p2 = 0
         ans = []
         while p1 < len1 and p2 < len2:
-            if word1[p1] > word2[p2]:
+            if word1[p1:] > word2[p2:]:
                 ans.append(word1[p1])
                 p1 += 1
-            elif word1[p1] < word2[p2]:
+            else:
                 ans.append(word2[p2])
                 p2 += 1
-            else:
-                temp1 = [word1[p1:], word2[p2:]]
-                if temp1 > temp1[::-1]:
-                    ans.append(word1[p1])
-                    p1 += 1
-                else:
-                    ans.append(word2[p2])
-                    p2 += 1
-        if p1 < len1:
-            ans.append(word1[p1:])
-        elif p2 < len2:
-            ans.append(word2[p2:])
-        return "".join(ans)
+        return "".join(ans) + word1[p1:] + word2[p2:]
