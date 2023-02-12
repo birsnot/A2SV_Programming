2d9ec733f2322c9d59a08682db9ed1ab14987c25
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-12 13:44:22 -0300

    Time: 296 ms (97.02%), Space: 18.9 MB (83.70%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -5,26 +5,14 @@
 #         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        cur = head
-        i = 0
-        while cur:
-            cur.val = (cur.val, i)
-            cur = cur.next
-            i += 1
-        ans = [0]*i
-        cur = dummy = ListNode(0, head)
-        prev = None
-        while cur.next.next:
-            val = cur.next.next.val[0]
-            if cur.next.val[0] < val:
-                ans[cur.next.val[1]] = val
-                cur.next = cur.next.next
-                while prev and prev.val[0] < val:
-                    ans[prev.val[1]] = val
-                    prev = prev.next
-            else:
-                temp = cur.next
-                cur.next = cur.next.next
-                temp.next = prev
-                prev = temp
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+        ans = [0]*len(nums)
+        monostack = []
+        for i, n in enumerate(nums):
+            while monostack and nums[monostack[-1]] < n:
+                ans[monostack.pop()] = n
+            monostack.append(i)
         return ans
