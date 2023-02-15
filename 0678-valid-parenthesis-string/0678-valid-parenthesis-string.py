0f6bdc1d5fce3de00697d48fc034fc4835c2410b
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-15 16:31:52 -0300

    Time: 19 ms (99.86%), Space: 13.9 MB (62.48%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,25 +1,15 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         count = 0
-        opening = 0
         for ch in s:
-            if ch == "(":
-                opening += 1
-            elif ch == "*":
-                count += 1
-            else:
-                if opening: opening -= 1
-                elif count: count -= 1
-                else: return False
-        count = 0
-        closing = 0
-        for ch in s[::-1]:
             if ch == ")":
-                closing += 1
-            elif ch == "*":
-                count += 1
-            else:
-                if closing: closing -= 1
-                elif count: count -= 1
+                if count: count -= 1
                 else: return False
+            else: count += 1
+        count = 0
+        for ch in s[::-1]:
+            if ch == "(":
+                if count: count -= 1
+                else: return False
+            else: count += 1
         return True
