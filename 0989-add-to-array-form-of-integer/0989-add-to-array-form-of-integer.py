4be0751dbc18303be5032b0ee7082a6754dae505
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-15 11:14:06 -0300

    Time: 277 ms (92.44%), Space: 14.9 MB (85.98%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,3 +1,12 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        return map(int, str(int("".join(map(str, num)))+k))
+        num[-1] += k
+        c = 0
+        N = len(num)-1
+        for i in range(N, 0, -1):
+            c, num[i] = divmod(num[i]+c, 10)
+            if not c:
+                break
+        c, num[0] = divmod(num[0]+c, 10)
+        if c: return list(map(int, str(c))) + num
+        return num
