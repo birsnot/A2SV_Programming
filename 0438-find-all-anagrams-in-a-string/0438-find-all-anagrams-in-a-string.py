15bf7f2d39d08889623b0ba854320d1e45aa6807
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-22 09:15:00 -0300

    Time: 86 ms (97.75%), Space: 15 MB (97.46%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,32 +1,16 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s): return []
-        p_count = {}
+        target = 0
         for ch in p:
-            p_count[ch] = p_count.get(ch, 0) + 1
+            target += hash(ch)
+        check = 0
         N = len(p)
-        n = 0
-        ch_count = {}
-        for key in p_count:
-            ch_count[key] = 0
+        for ch in s[:N]:
+            check += hash(ch)
         ans = []
-        i = 0
-        for j, ch in enumerate(s):
-            if ch not in ch_count:
-                i = j+1
-                for key in ch_count:
-                    ch_count[key] = 0
-                    n = 0
-            else:
-                if ch_count[ch] == p_count[ch]:
-                    while s[i] != ch:
-                        ch_count[s[i]] -= 1
-                        i += 1
-                        n -= 1
-                    i += 1
-                else:
-                    ch_count[ch] += 1
-                    n += 1
-                if n == N:
-                    ans.append(i)
+        if check == target: ans.append(0)
+        for i, ch in enumerate(s[N:]):
+            check += hash(ch) - hash(s[i])
+            if check == target:
+                ans.append(i+1)
         return ans
