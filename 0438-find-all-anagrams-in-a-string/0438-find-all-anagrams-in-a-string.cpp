7c5c056ba393c62c3173f75e091a3ba33d2b7801
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-23 07:04:56 -0300

    Time: 14 ms (70.27%), Space: 8.8 MB (41.97%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -4,44 +4,38 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         
-        unordered_map<char, int> pChars;
-        for(int i = 0; i < p.length(); ++i){
-            pChars[p[i]] += 1;
+        int p_count[128] = {};
+        for (int i = 0; i < p.length(); ++i){
+            p_count[p[i]] += 1;
         }
-        unordered_map<char, int> check;
-        for(auto ch: pChars){
-            check[ch.first] = 0;
-        }
+        int s_count[128] = {};
+        int count = 0, l = 0;
         vector <int> ans;
-        int count = 0;
-        int l = 0;
         for(int i = 0; i < s.length(); ++i){
-            if (check.find(s[i]) != check.end()){
-                if(check[s[i]] < pChars[s[i]]){
-                    ++check[s[i]];
+            if (p_count[s[i]] == 0){
+                for(int j = 0; j < 128; ++j) s_count[j] = 0;
+                l = i + 1;
+                count = 0;
+            }
+            else{
+                if(p_count[s[i]] > s_count[s[i]]){
                     ++count;
+                    ++s_count[s[i]];
                     if (count == p.length()){
                         ans.push_back(l);
-                        --check[s[l]];
                         --count;
+                        --s_count[s[l]];
                         ++l;
                     }
                 }
                 else{
                     while(s[l] != s[i]){
-                        --check[s[l]];
-                        --count;
+                        --s_count[s[l]];
                         ++l;
+                        --count;
                     }
                     ++l;
                 }
-            }
-            else{
-                for(auto ch: pChars){
-                    check[ch.first] = 0;
-                }
-                count = 0;
-                l = i + 1;
             }
         }
         return ans;
