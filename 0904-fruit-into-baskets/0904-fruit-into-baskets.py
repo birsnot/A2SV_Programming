904f09042d9a3a72cbf6483268dd65ab54e3a3ab
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-27 08:47:38 -0300

    Time: 813 ms (91.74%), Space: 20 MB (84.28%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,33 +1,13 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        fruit = [-1, -1, -1, -1]
-        N = len(fruits)
-        l = r = 0
-        while r < N and -1 in fruit:
-            if fruits[r] == fruit[0]:
-                fruit[1] = r
-            elif fruit[0] == -1:
-                fruit[0] = fruits[r]
-                fruit[1] = r
-            else:
-                fruit[2] = fruits[r]
-                fruit[3] = r
-            r += 1
-        ans = r - l        
-        while r < N:
-            if fruits[r] == fruit[0]:
-                fruit[1] = r
-            elif fruits[r] == fruit[2]:
-                fruit[3] = r
-            else:
-                if fruit[1] < fruit[3]:
-                    l = fruit[1] + 1
-                    fruit[0] = fruits[r]
-                    fruit[1] = r
+        my_fruits = defaultdict(int)
+        l = 0
+        for fr in fruits:
+            my_fruits[fr] += 1
+            if len(my_fruits) > 2:
+                if my_fruits[fruits[l]] == 1:
+                    del my_fruits[fruits[l]]
                 else:
-                    l = fruit[3] + 1
-                    fruit[2] = fruits[r]
-                    fruit[3] = r
-            r += 1
-            ans = max(ans, r - l)
-        return ans
+                    my_fruits[fruits[l]] -= 1
+                l += 1
+        return len(fruits) - l
