893ec552881b78d2652d013afa83f482d63d6673
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-27 10:36:25 -0300

    Time: 97 ms (65.53%), Space: 47.1 MB (21.62%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> match;
+        match[0] = 1;
+        int ans = 0;
+        long sum_ = 0;
+        for(auto n: nums){
+            sum_ += n;
+            ans += match[sum_ - k];
+            match[sum_] += 1;
+        }
+        return ans;
+    }
+};
