e5c8310b66ead82b1940283b49d7144681726fe7
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-28 04:26:13 -0300

    Time: 38 ms (41.69%), Space: 13.8 MB (93.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,7 +1,4 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n < 1: return False
-        binary = bin(n)
-        if binary.count("1") == 1 and (len(binary) - binary.index("1") - 1)%2 == 0:
-            return True
-        return False
+        return log(n, 4).is_integer()
