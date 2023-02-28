69e77411929cfb9b4c132c72161cb782370ac72f
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-28 11:18:53 -0300

    Time: 408 ms (36.46%), Space: 104.5 MB (11.60%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int N = s.length() + 1;
+        vector <int> accu(N);
+        for(auto shift: shifts){
+            if(shift[2]){
+                accu[shift[0]] += 1;
+                accu[shift[1] + 1] -= 1;
+            }
+            else{
+                accu[shift[0]] -= 1;
+                accu[shift[1] + 1] += 1;
+            }
+        }
+        for(int i = 1; i < N; ++i){
+            accu[i] += accu[i - 1];
+        }
+        string chars = "abcdefghijklmnopqrstuvwxyz";
+        vector <char> ans(s.length());
+        for(int i = 0; i < s.length(); ++i){
+            int idx = (int)s[i] - 97 + accu[i];
+            idx = (idx%26 + 26)%26;
+            ans[i] = chars[idx];
+        }
+        return string(ans.begin(), ans.end());
+    }
+};
