b59483d2c4607366f16d7401572f6f5bab905084
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-02-28 11:21:53 -0300

    Time: 336 ms (91.99%), Space: 93.1 MB (94.75%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -3,7 +3,7 @@ public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int N = s.length() + 1;
         vector <int> accu(N);
-        for(auto shift: shifts){
+        for(auto &shift: shifts){
             if(shift[2]){
                 accu[shift[0]] += 1;
                 accu[shift[1] + 1] -= 1;
@@ -17,12 +17,11 @@ public:
             accu[i] += accu[i - 1];
         }
         string chars = "abcdefghijklmnopqrstuvwxyz";
-        vector <char> ans(s.length());
         for(int i = 0; i < s.length(); ++i){
             int idx = (int)s[i] - 97 + accu[i];
             idx = (idx%26 + 26)%26;
-            ans[i] = chars[idx];
+            s[i] = chars[idx];
         }
-        return string(ans.begin(), ans.end());
+        return s;
     }
 };
