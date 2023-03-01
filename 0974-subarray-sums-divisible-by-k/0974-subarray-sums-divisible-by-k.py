7d904f0c32fcc08cd5371e156e8612e912dda8ce
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-03-01 03:46:20 -0300

    Time: 292 ms (84.84%), Space: 18.7 MB (86.03%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,13 +1,11 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        sofar = {0: 1}
+        sofar = [0]*k
+        sofar[0] = 1
         sum_ = ans = 0
         for n in nums:
             sum_ += n
             check = sum_%k
-            if check in sofar:
-                ans += sofar[check]
-                sofar[check] += 1
-            else:
-                sofar[check] = 1
+            ans += sofar[check]
+            sofar[check] += 1
         return ans
