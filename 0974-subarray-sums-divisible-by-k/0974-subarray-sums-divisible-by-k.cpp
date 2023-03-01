9ae95be046b5418e9eb0dab7bb80b9cbcbb7064c
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-03-01 03:55:56 -0300

    Time: 48 ms (71.61%), Space: 30.2 MB (75.29%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        vector <int> sofar(k);
-        sofar[0] = 1;
-        long sum_ = 0, ans = 0, mod;
-        for(auto n: nums){
-            sum_ += n;
-            mod = (sum_%k + k)%k;
-            ans += sofar[mod]++;
+         vector<int> vals(k,0);
+        vals[0] = 1;
+        int total = 0;
+
+
+        for(auto j:nums)
+        {
+               total = total + (j%k + k)%k;
+
+              vals[total%k]++;
+
         }
-        return ans;
+
+        int res = 0;
+        for(auto m:vals)
+        {
+              res = res + (m*(m-1))/2;
+        }
+
+        return res;
     }
 };
