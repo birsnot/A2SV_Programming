b5ad0eccdf265b0d6b741ff62970823fa5494fb1
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-03-15 15:41:39 -0300

    added binary-subarrays-with-sum.python

--- binary-subarrays-with-sum.py
+++ binary-subarrays-with-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        sum_ = 0
-        sums = {}
+        ans = sum_ = 0
+        sums = defaultdict(int)
         sums[0] = 1
-        ans = 0
         for n in nums:
             sum_ += n
-            check = sum_ - goal
-            if check in sums:
-                ans += sums[check]
-            sums[sum_] = sums.get(sum_, 0) + 1
+            ans += sums[sum_ - goal]
+            sums[sum_] += 1
         return ans
