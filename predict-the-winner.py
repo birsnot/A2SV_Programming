1797916fd5466e4489195b581fb291fa92a5204b
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-03-15 06:08:08 -0300

    added predict-the-winner.python

--- predict-the-winner.py
+++ predict-the-winner.py
@@ -1,10 +1,11 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        def score(nums, l, r):
-            if l == r:
-                return nums[l]
-            left = nums[l] - score(nums, l + 1, r)
-            right = nums[r] - score(nums, l, r - 1)
-            return max(left, right)
-        s = score(nums, 0, len(nums)-1)
+        s = self.score(nums, 0, len(nums)-1)
         return s >= 0
+    
+    def score(self, nums, l, r):
+        if l == r:
+            return nums[l]
+        left = nums[l] - self.score(nums, l + 1, r)
+        right = nums[r] - self.score(nums, l, r - 1)
+        return max(left, right)
