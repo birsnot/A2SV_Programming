fe1b4df85380329aeceeefc013b6b1c526c569b3
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-03-22 06:32:18 -0300

    added shortest-subarray-with-sum-at-least-k.python

--- shortest-subarray-with-sum-at-least-k.py
+++ shortest-subarray-with-sum-at-least-k.py
@@ -1,17 +1,14 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        q = deque([(-1,0)])
-        sums = 0
-        res = len(nums) + 1
-
-        for i,n in enumerate(nums):
-            sums += n
-            if n > 0:
-                target = sums-k
-                while q and q[0][1] <= target:
-                    res = min(res,i-q.popleft()[0])
-            else:
-                while q and sums <= q[-1][1]:
-                    q.pop()
-            q.append((i,sums))
-        return -1 if res > len(nums) else res
+        ps = list(accumulate(nums, initial=0))
+        stk = deque()
+        ans = len(nums) + 1
+        for i, n in enumerate(ps):
+            while stk and n - ps[stk[0]] >= k:
+                ans = min(ans, i - stk.popleft())
+            while stk and ps[stk[-1]] >= n:
+                stk.pop()
+            stk.append(i)
+        if ans > len(nums):
+            return -1
+        return ans
