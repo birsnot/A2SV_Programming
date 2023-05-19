930a5fc0a2e3a1b89befc9de425b2b4e1b4047d8
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-05-19 12:53:19 -0700

    added koxia-and-whiteboards.py

---
+++ koxia-and-whiteboards.py
@@ -0,0 +1,36 @@
+# author: birsnot - Nardos 
+
+
+from sys import setrecursionlimit, stdin
+from threading import Thread, stack_size
+# def input(): return stdin.readline()[:-1]
+input = stdin.readline
+def I(): return int(input())
+def II(): return map(int, input().split())
+def IL(): return list(map(int, input().split()))
+def SIL(): return sorted(map(int, input().split()))
+
+def solve():
+    N, M = II()
+    nums = IL()
+    bs = IL()
+    ans = bs.pop()
+    nums += bs
+    nums.sort(reverse=True)
+    ans += sum(nums[:N - 1])
+    print(ans)
+    
+
+def main():
+    T = I()
+    # T = 1
+    for ___ in range(T):
+        solve()
+
+main()
+
+# setrecursionlimit(100050)
+# stack_size(1<<27)
+# _mt = Thread(target=main)
+# _mt.start()
+# _mt.join()
