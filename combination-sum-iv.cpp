9582983127713027caf5d2bb2347a44cab45f67b
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-06-02 07:12:10 -0700

    added combination-sum-iv.cpp

---
+++ combination-sum-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        unsigned int dp[target + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i <= target; ++i){
+            for(auto n: nums){
+                if(n <= i){
+                    dp[i] += dp[i - n];
+                }
+                else break;
+            }
+        }
+        return dp[target];
+    }
+};
