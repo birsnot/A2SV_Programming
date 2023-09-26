f88d53b2bc04a654bdb5cad2ea59fe46ddd9752c
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-09-26 10:51:12 -0300

    added last-stone-weight-ii.python

---
+++ last-stone-weight-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        dp = {0}
+        for n in stones:
+            cur = set()
+            for prev in dp:
+                cur.add(prev + n)
+            dp.update(cur)
+        sum_ = sum(stones)
+        target = sum_//2
+        for i in range(100):
+            cur = target - i
+            if cur in dp:
+                return sum_ - 2*cur
+            cur = target + i
+            if cur in dp:
+                return sum_ - 2*cur
