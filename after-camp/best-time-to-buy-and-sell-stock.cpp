6075c3d4cf40f93f5d75c4244153fbc757267e92
Nardos Wehabe <104576546+birsnot@users.noreply.github.com>
2023-11-24 16:08:58 -0300

    Added solution for Best Time to Buy and Sell Stock

--- after-camp/best-time-to-buy-and-sell-stock.cpp
+++ after-camp/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_ = INT_MAX, ans = INT_MIN;
-        for(auto p: prices){
-            ans = max(ans, p - min_);
-            min_ = min(p, min_);
+        int min_ = INT_MAX, ans = 0;
+        for(auto price: prices){
+            ans = max(ans, price - min_);
+            min_ = min(min_, price);
         }
-        return max(0, ans);
+        return ans;
     }
 };
